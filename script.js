68694473dc1add4d0a8903eef8cbb58463a770f4
Irgis Valentino <101648426+irgisvalen@users.noreply.github.com>
2023-04-26 01:26:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ function resetTypewriter() {
   document.getElementById("text").innerHTML = "";
   clearInterval(intervalId);
   i = 0;
-  ucapanSurat = "Taqabbalallahu Minna Wa Minkum \n Minal Aidzin Wal Faidzin \n Mohon Maaf Lahir dan Batin🙏 \n Jangan Lupa THRnya ya \n ❤️😁";
+  ucapanSurat = "Taqabbalallahu Minna Wa Minkum \n Minal Aidzin Wal Faidzin \n Mohon Maaf Lahir dan Batin🙏 \n Jangan Lupa THRnya ya \n 🫰🏻😁";
 }
 
 function typewriter() {
